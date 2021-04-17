c58e864338eb3bce08c3a2cdfdbe8d8e9261f8bd
becooni <junghum21@gmail.com>
2021-04-18 00:17:55 +0900

    Add takeUntil

--- app/src/main/java/com/becooni/rxexam/MainActivity.kt
+++ app/src/main/java/com/becooni/rxexam/MainActivity.kt
@@ -3,7 +3,7 @@ package com.becooni.rxexam
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.becooni.rxexam.databinding.ActivityMainBinding
-import com.becooni.rxexam.operator.conditional.amb
+import com.becooni.rxexam.operator.conditional.takeUntil
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +15,6 @@ class MainActivity : AppCompatActivity() {
 
         start()
 
-        amb()
+        takeUntil()
     }
 }
---
+++ app/src/main/java/com/becooni/rxexam/operator/conditional/TakeUntil.kt
@@ -0,0 +1,19 @@
+package com.becooni.rxexam.operator.conditional
+
+import com.becooni.rxexam.log
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import java.util.concurrent.TimeUnit
+
+fun takeUntil() {
+    val data = listOf("1", "2", "3", "4", "5", "6")
+
+    val source = Observable.fromIterable(data)
+        .zipWith(
+            Observable.interval(100L, TimeUnit.MILLISECONDS),
+            { value, _ -> value }
+        )
+        .takeUntil(Observable.timer(500L, TimeUnit.MILLISECONDS))
+
+    source.subscribeBy(onNext = ::log)
+}
